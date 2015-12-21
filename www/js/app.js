e64f164cde58e915cd8d2b78f2ef08bddedf5a7d
marb <marb61a@gmail.com>
2015-12-21 18:00:41 +0000

    updated app.js functions

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 function onDeviceReady(){
     var channel = '';
     
-    getPlayList(channel);
+    getPlaylist(channel);
 }
 
 function getPlaylist(channel){
@@ -14,7 +14,15 @@ function getPlaylist(channel){
     $.get(
         "https://www.googleapis.com/youtube/v3/channels",
         {
-            
+            part: 'contentDetails',
+				forUsername: channel,
+				key: ''
+        }, function(data){
+            $.each(data.items, function(i, item){
+					console.log(item);
+					playlistId = item.contentDetails.relatedPlaylists.uploads;
+					getVideos(playlistId, 10);
+				});
         }
         );
 }
@@ -23,7 +31,19 @@ function getVideos(playlistId, maxResults){
     $.get(
         "https://www.googleapis.com/youtube/v3/playlistItems",
         {
-            
+            part: 'snippet',
+			maxResults: maxResults,
+			playlistId: playlistId,
+			key:''
+        }, function(data){
+            var output;
+			$.each(data.items, function(i, item){
+				id = item.snippet.resourceId.videoId;
+				title = item.snippet.title;
+				thumb = item.snippet.thumbnails.default.url;
+				$('#vidlist').append('<li videoId="'+id+'"><img src="'+thumb+'"><h3>'+title+'</h3></li>');
+				$('#vidlist').listview('refresh');
+			});
         }
         );
 }
